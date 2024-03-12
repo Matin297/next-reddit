317f74eef10d2a7a4f8fe3d0a5de05e2202e44a9
Matin Mir <asmamir297@gmail.com>
2024-03-12 16:01:40 +0330

    lib: add pathname methods for pages upfront

---
+++ app/lib/paths.ts
@@ -0,0 +1,17 @@
+const pathnames = {
+  home() {
+    return "/";
+  },
+  topic(slug: string) {
+    return `/topics/${slug}`;
+  },
+  topicCreation() {
+    return `/topics/create`;
+  },
+  post(slug: string, id: string) {
+    return `/topics/${slug}/posts/${id}`;
+  },
+  postCreation(slug: string) {
+    return `/topics/${slug}/posts/create`;
+  },
+};
