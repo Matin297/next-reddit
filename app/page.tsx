9afb9763446b86d6714471c1f74b6c3021ae2ed3
Matin Mir <asmamir297@gmail.com>
2024-03-15 11:10:26 +0330

    home: remove unused import statements

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@nextui-org/react";
-import { signIn, signOut } from "@/app/lib/actions";
-
 import ProfileClient from "@/app/components/profile-client";
 import ProfileServer from "@/app/components/profile-server";
 
