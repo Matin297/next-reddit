62ebe278e795834422e690fd23f97b02814936a6
Matin Mir <asmamir297@gmail.com>
2024-03-15 11:13:27 +0330

    home: remove example placeholder content

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import ProfileClient from "@/app/components/profile-client";
-import ProfileServer from "@/app/components/profile-server";
-
 export default function Home() {
-  return (
-    <div>
-      <ProfileClient />
-      <ProfileServer />
-    </div>
-  );
+  return <div>Home Page</div>;
 }
