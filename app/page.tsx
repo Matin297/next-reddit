89deb9653fa68298a7b9172e60dd2ae770653466
Matin Mir <asmamir297@gmail.com>
2024-03-17 13:03:58 +0330

    global: add create topic button link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
+import { pathnames } from "@/app/lib/paths";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Button as={Link} color="primary" href={pathnames.topicCreation()}>
+        Create Topic
+      </Button>
+    </div>
+  );
 }
