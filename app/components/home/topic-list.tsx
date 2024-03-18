096342d75649c1223ecff4d2ffeeb072f2a2ad8a
Matin Mir <asmamir297@gmail.com>
2024-03-18 12:41:58 +0330

    home: fetch and display all topics as links

---
+++ app/components/home/topic-list.tsx
@@ -0,0 +1,27 @@
+import NextLink from "next/link";
+import { Link } from "@nextui-org/react";
+import { fetchTopics } from "@/app/lib/data";
+import { pathnames } from "@/app/lib/paths";
+
+export default async function TopicList() {
+  const topics = await fetchTopics();
+
+  return (
+    <section className="border rounded-lg p-3">
+      <h2 className="text-lg font-semibold mb-4">Topics</h2>
+      <ul>
+        {topics.map((topic) => (
+          <li key={topic.id}>
+            <Link
+              underline="hover"
+              as={NextLink}
+              href={pathnames.topic(topic.slug)}
+            >
+              {topic.slug}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
