1e5e00abbcc4dce18587544c5afe14b60b651d1d
Matin Mir <asmamir297@gmail.com>
2024-03-18 12:42:42 +0330

    home: structure posts and topics along side each other

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
 import { Button } from "@nextui-org/react";
 import { pathnames } from "@/app/lib/paths";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import TopicList from "@/app/components/home/topic-list";
 
 export default function Home() {
   return (
-    <div>
-      <Button as={Link} color="primary" href={pathnames.topicCreation()}>
-        Create Topic
-      </Button>
-    </div>
+    <section className="flex">
+      <ul className="flex-1">
+        <li>Here goes the list of posts</li>
+      </ul>
+      <aside className="space-y-4 basis-1/4 flex flex-col">
+        <Button
+          as={Link}
+          color="primary"
+          className="self-end"
+          href={pathnames.topicCreation()}
+        >
+          <PlusIcon width={20} />
+          Create Topic
+        </Button>
+        <TopicList />
+      </aside>
+    </section>
   );
 }
