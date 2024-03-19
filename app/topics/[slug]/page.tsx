3cfdf9ee695aade2459939f57688901a48e0de1b
Matin Mir <asmamir297@gmail.com>
2024-03-19 13:13:32 +0330

    topic: create the main layout structure with a link to create a post

--- app/topics/[slug]/page.tsx
+++ app/topics/[slug]/page.tsx
@@ -1,3 +1,35 @@
-export default function Topic() {
-  return <p>Here is the topic specific page</p>;
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
+import { pathnames } from "@/app/lib/paths";
+import { PlusIcon } from "@heroicons/react/24/outline";
+
+interface TopicProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function Topic({ params: { slug } }: TopicProps) {
+  return (
+    <section className="flex">
+      <ul className="flex-1">
+        <li>Here goes the list of posts</li>
+      </ul>
+      <aside className="space-y-4 basis-1/4 flex flex-col">
+        <Button
+          as={Link}
+          color="primary"
+          className="self-end"
+          href={pathnames.postCreation(slug)}
+        >
+          <PlusIcon width={20} />
+          Create Post
+        </Button>
+        <section className="border p-3 rounded-lg">
+          <h2 className="text-lg font-semibold capitalize mb-4">{slug}</h2>
+          <p>Here goes the topic description</p>
+        </section>
+      </aside>
+    </section>
+  );
 }
