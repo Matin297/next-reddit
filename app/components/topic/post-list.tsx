e84c422cb3b1daece0db0e2e76ad3f8a4922e777
Matin Mir <asmamir297@gmail.com>
2024-03-20 16:25:08 +0330

    components: implement post list component ui

---
+++ app/components/topic/post-list.tsx
@@ -0,0 +1,49 @@
+import NextLink from "next/link";
+import { Link } from "@nextui-org/react";
+import {
+  ChatBubbleBottomCenterTextIcon,
+  LinkIcon,
+} from "@heroicons/react/24/outline";
+import { pathnames } from "@/app/lib/paths";
+import { EnhancedPostItem } from "@/app/lib/data";
+
+interface PostListProps {
+  fetchPosts: () => Promise<EnhancedPostItem[]>;
+}
+
+export default async function PostList({ fetchPosts }: PostListProps) {
+  const posts = await fetchPosts();
+
+  return (
+    <ul className="flex-1 divide-y">
+      {posts.map((post) => (
+        <li key={post.id} className="py-3 space-y-4">
+          <Link
+            as={NextLink}
+            underline="hover"
+            color="foreground"
+            className="space-x-2"
+            href={pathnames.post(post.topic.slug, post.id)}
+          >
+            <LinkIcon width={15} className="text-slate-500" />
+            <h3 className="text-md font-semibold">{post.title}</h3>
+          </Link>
+          <p>{post.content.substring(20)}</p>
+          <section className="flex justify-end gap-4">
+            <span className="space-x-2">
+              <span className="text-slate-500 text-sm">By</span>
+              <span>{post.user.name}</span>
+            </span>
+            <span className="flex gap-2">
+              <ChatBubbleBottomCenterTextIcon
+                className="text-slate-500"
+                width={20}
+              />
+              <span>{post._count.comments}</span>
+            </span>
+          </section>
+        </li>
+      ))}
+    </ul>
+  );
+}
