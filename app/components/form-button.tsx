aa7830fb065a1611449dd00425c3f7f297d1d2f0
Matin Mir <asmamir297@gmail.com>
2024-03-21 17:22:22 +0330

    components: accept className as a prop for customization

--- app/components/form-button.tsx
+++ app/components/form-button.tsx
@@ -1,13 +1,17 @@
 import { useFormStatus } from "react-dom";
 import { Button } from "@nextui-org/react";
 
-export default function FormButton() {
+interface FormButtonProps {
+  className?: string;
+}
+
+export default function FormButton({ className = "" }: FormButtonProps) {
   const { pending } = useFormStatus();
 
   return (
     <Button
       color="primary"
-      className="w-full"
+      className={`w-full ${className}`}
       type="submit"
       aria-disabled={pending}
       isLoading={pending}
