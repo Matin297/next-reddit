cce96ccb8bb0a0c783b8ebbb45d18728eea5bb7b
Matin Mir <asmamir297@gmail.com>
2024-03-22 17:52:36 +0330

    post: render details, comment form and comment list sections

--- app/topics/[slug]/posts/[id]/page.tsx
+++ app/topics/[slug]/posts/[id]/page.tsx
@@ -1,3 +1,21 @@
-export default function PostDetails() {
-  return <p>Here goes the details of a chosen post</p>;
+import { Divider } from "@nextui-org/react";
+import Details from "@/app/components/post/details";
+import CommentForm from "@/app/components/post/comment-form";
+import CommentList from "@/app/components/post/comment-list";
+
+interface PostDetailsProps {
+  params: {
+    slug: string;
+    id: string;
+  };
+}
+
+export default function PostDetails({ params: { id } }: PostDetailsProps) {
+  return (
+    <section className="space-y-4">
+      <Details postID={id} />
+      <CommentForm mode="comment" postID={id} />
+      <CommentList postID={id} />
+    </section>
+  );
 }
