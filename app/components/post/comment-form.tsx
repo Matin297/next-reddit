f58e1c82103094ac0da19b5bec2642e7e020fe73
Matin Mir <asmamir297@gmail.com>
2024-03-22 17:51:40 +0330

    components: adjust comment form to work with reply functionality

--- app/components/post/comment-form.tsx
+++ app/components/post/comment-form.tsx
@@ -1,34 +1,81 @@
 "use client";
 
+import { useRef, useEffect, useState } from "react";
 import { useFormState } from "react-dom";
-import { Textarea } from "@nextui-org/react";
+import { Textarea, Button } from "@nextui-org/react";
 import { createComment } from "@/app/lib/actions";
 import FormButton from "@/app/components/form-button";
 import Alert from "@/app/components/alert";
 
 interface CommentFormProps {
   postID: string;
-  slug: string;
+  parentId?: string;
+  mode: "comment" | "reply";
 }
 
-export default function CommentForm({ postID, slug }: CommentFormProps) {
+export default function CommentForm({
+  mode,
+  postID,
+  parentId,
+}: CommentFormProps) {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [isOpen, setIsOpen] = useState(mode === "comment");
   const [state, action] = useFormState(createComment, {});
 
+  useEffect(() => {
+    if (state.status === "success") {
+      formRef.current?.reset();
+
+      if (mode === "reply") {
+        setIsOpen(false);
+      }
+    }
+  }, [state.status, mode]);
+
+  if (!isOpen && mode === "reply") {
+    return (
+      <Button
+        color="primary"
+        variant="bordered"
+        className="self-end"
+        onClick={() => setIsOpen(true)}
+      >
+        Reply
+      </Button>
+    );
+  }
+
   return (
-    <form action={action} className="flex flex-col gap-4">
-      <h2 className="text-lg font-semibold">What are you thoughts?</h2>
+    <form ref={formRef} action={action} className="flex flex-col gap-4">
+      {mode === "comment" && (
+        <h2 className="text-lg font-semibold">What are you thoughts?</h2>
+      )}
       <Alert message={state.message} />
       <Textarea
         required
-        variant="bordered"
         name="content"
         label="Comment"
         isInvalid={Boolean(state.errors?.content)}
         errorMessage={state.errors?.content?.join(", ")}
       />
-      <input type="hidden" name="postId" value={postID} />
-      <input type="hidden" name="slug" value={slug} />
-      <FormButton className="w-auto self-end" />
+      <input type="hidden" name="postId" value={postID} required />
+      {parentId && (
+        <input type="hidden" name="parentId" value={parentId} required />
+      )}
+
+      <div className="space-x-4 self-end">
+        {mode === "reply" && (
+          <Button
+            variant="light"
+            color="danger"
+            onClick={() => setIsOpen(false)}
+          >
+            Cancel
+          </Button>
+        )}
+        <FormButton className="w-auto" />
+      </div>
     </form>
   );
 }
