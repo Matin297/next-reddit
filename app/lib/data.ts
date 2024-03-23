e3e80b14a46187c3ed3909418810805ab194d4f1
Matin Mir <asmamir297@gmail.com>
2024-03-23 23:29:59 +0330

    lib: implement fetch top posts data function

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,7 +30,7 @@ export type EnhancedPostItem = Awaited<
   ReturnType<typeof fetchPostsBySlug>
 >[number];
 
-export async function fetchPostsBySlug(slug?: string) {
+export async function fetchPostsBySlug(slug: string) {
   try {
     const posts = await db.post.findMany({
       where: { topic: { slug } },
@@ -47,6 +47,31 @@ export async function fetchPostsBySlug(slug?: string) {
     return posts;
   } catch (error) {
     throw new Error("Failed to fetch posts!");
+  }
+}
+
+export async function fetchTopPosts() {
+  try {
+    const posts = await db.post.findMany({
+      orderBy: {
+        comments: {
+          _count: "desc",
+        },
+      },
+      include: {
+        topic: { select: { slug: true } },
+        user: { select: { name: true } },
+        _count: {
+          select: {
+            comments: true,
+          },
+        },
+      },
+      take: 10,
+    });
+    return posts;
+  } catch (error) {
+    throw new Error("Failed to fetch top posts!");
   }
 }
 
