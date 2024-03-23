d66e86f2529bdc26bfe504b30c16294761c62d59
Matin Mir <asmamir297@gmail.com>
2024-03-23 23:31:04 +0330

    home: display top 10 most commented on posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
+import { fetchTopPosts } from "@/app/lib/data";
 import { Button } from "@nextui-org/react";
 import { pathnames } from "@/app/lib/paths";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import TopicList from "@/app/components/home/topic-list";
+import PostList from "@/app/components/topic/post-list";
 
 export default function Home() {
   return (
-    <section className="flex">
-      <ul className="flex-1">
-        <li>Here goes the list of posts</li>
-      </ul>
+    <section className="flex gap-4">
+      <PostList fetchPosts={fetchTopPosts} />
       <aside className="space-y-4 basis-1/4 flex flex-col">
         <Button
           as={Link}
